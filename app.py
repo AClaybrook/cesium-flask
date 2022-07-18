cea9fadf18156333789e451cb38a039e31e7274d
AClaybrook <48696805+AClaybrook@users.noreply.github.com>
2022-07-18 02:57:42 -0700

    Load as primitives instead of entities

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file, send_from_directory
 
 # Initialize app
 app = Flask(__name__)
 
-# Add root of websire
+# Add root of website
 @app.route('/')
 def home():
     return render_template('index.html',data={'online':True})
+
+# Return czml
+@app.route('/czml')
+def get_czml():
+    filename = 'data/data.czml'
+    return send_file(filename)
+
+# Return data 
+@app.route('/json')
+def get_json():
+    filename = 'data/data.json'
+    return send_file(filename)
 
 # Run if main
 if __name__ == "__main__":
--- static/js/CesiumContainer.js
+++ static/js/CesiumContainer.js
@@ -2,7 +2,7 @@
 //   Cesium.Ion.defaultAccessToken = ''
 
 // Inputs
-var online = false
+var online = true
 
 // Create now cesium container
 if(online==true){
@@ -12,8 +12,6 @@ if(online==true){
     });
     // Add in Open Street Map buildings
     viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
-
 } else {
     // Stick to default globe
     var viewer = new Cesium.Viewer('cesiumContainer',{
@@ -23,12 +21,243 @@ if(online==true){
     });
 }
 
+viewer.scene.globe.enableLighting = true;
+
+
+
+// Webpage controls
+
+
+// 
+var camera = viewer.camera
+var scene = viewer.scene
 
 
 // Clock options
-// Set clock multipler of X real time
-viewer.clock.multiplier = 30
+var now = new Date().toISOString();
+var startDate = now.split('T')[0]
+var startTime = new Cesium.JulianDate.fromIso8601(startDate)
+function initializeClock(){
+    viewer.clock.multiplier = 1 // Multiple of X realtime
+    viewer.clock.shouldAnimate = false
+    viewer.clockViewModel.currentTime = startTime;
+    viewer.timeline.updateFromClock();
+}
 
-// Set to animate
-viewer.clock.shouldAnimate = true
+function setTimeNow(){
+  var now = new Date().toISOString();
+  var time = new Cesium.JulianDate.fromIso8601(now)
+  viewer.clock.multiplier = 1 // Multiple of X realtime
+  viewer.clock.shouldAnimate = true
+  viewer.clockViewModel.currentTime = time;
+  viewer.timeline.updateFromClock();
+}
+
+initializeClock()
+
+function addToolbarButton(text, onclick, toolbarID) {
+    var button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'cesium-button';
+    button.onclick = function() {
+        reset();
+        onclick();
+    };
+    button.textContent = text;
+    document.getElementById(toolbarID || 'toolbar').appendChild(button);
+}
+
+function addToolbarMenu(options, toolbarID) {
+    var menu = document.createElement('select');
+    menu.className = 'cesium-button';
+    menu.onchange = function() {
+        window.Sandcastle.reset();
+        var item = options[menu.selectedIndex];
+        if (item && typeof item.onselect === 'function') {
+            item.onselect();
+        }
+    };
+    document.getElementById(toolbarID || 'toolbar').appendChild(menu);
+
+    for (var i = 0, len = options.length; i < len; ++i) {
+        var option = document.createElement('option');
+        option.textContent = options[i].text;
+        option.value = options[i].value;
+        menu.appendChild(option);
+    }
+}
+
+
+function reset() {
+    viewer.dataSources.removeAll();
+    viewer.entities.removeAll();
+    viewer.scene.primitives.removeAll();
+    initializeClock()
+}
+
+// Add buttons, the main app.py file serves these urls and are defined in the flask routes
+addToolbarButton("Reset", function () {
+    reset()
+})
+
+addToolbarButton("Set time now", function () {
+  setTimeNow()
+})
+
+addToolbarButton("Czml Datasource", function () {
+    viewer.clock.shouldAnimate = true
+    viewer.dataSources.add(Cesium.CzmlDataSource.load("czml"));
+});
+
+addToolbarButton("Lat Lon Val as Polyline", function () {
+    async function loadPloyline(jsonFilename){
+        const response = await fetch(jsonFilename)
+        const data = await response.json()
+    
+        const valueMin = -4.7
+        const valueMax = 6.7
+        const range = valueMax-valueMin
+        const scale = 1/range
+        const offset = 0
+        const sizeScale = 20/range
+        const sizeOffset = 5
+        const heightScale = 1000000/range
+        const heightOffset = 10000
+    
+
+        // Create a polyline collection 
+        // const polylines = viewer.scene.primitives.add(new Cesium.PolylineCollection());
+
+        for (dataPoint of data){
+          // Add a marker for this point
+          const {lon,lat,val} = dataPoint
+          const scaledVal = (val+valueMin)*scale+offset
+          const sizeScaledVal = (val-valueMin)*sizeScale+sizeOffset
+          const heightScaledVal = (val-valueMin)*heightScale+heightOffset
+    
+          const color = Cesium.Color.fromHsl(scaledVal, 0.8, 0.5)
+          const surfacePosition = Cesium.Cartesian3.fromDegrees(lon,lat,0);
+          const heightPosition = Cesium.Cartesian3.fromDegrees(lon,lat,heightScaledVal);
+    
+          // // WebGL Globe only contains lines, so that's the only graphics we create.
+          const polyline = new Cesium.PolylineGraphics();
+          polyline.material = new Cesium.ColorMaterialProperty(color);
+          polyline.width = new Cesium.ConstantProperty(5);
+          polyline.arcType = new Cesium.ConstantProperty(Cesium.ArcType.NONE);
+          polyline.positions = new Cesium.ConstantProperty([
+            surfacePosition,
+            heightPosition,
+          ]);
+    
+          //The polyline instance itself needs to be on an entity.
+          const entity = new Cesium.Entity({
+            polyline: polyline,
+            description:`Temp [C] ${val}`,
+          });
+    
+          viewer.entities.add(entity)
+
+          // const material = Cesium.Material.fromType('Color', {color : color});
+
+          // const polylineOptions = {
+          //   material: material,
+          //   width : 5,
+          //   positions : [surfacePosition,heightPosition]
+          // }
+          // polylines.add(polylineOptions)
+    
+        }
+
+      }
+
+
+    // Call from url
+    loadPloyline("/json")
+
+});
+
+addToolbarButton("Lat Lon Alt Val Scatter", function () {
+    async function loadScatter(jsonFilename){
+        const response = await fetch(jsonFilename)
+        const data = await response.json()
+    
+        const valueMin = -4.7
+        const valueMax = 6.7
+        const range = valueMax-valueMin
+        const scale = 1/range
+        const offset = 0
+        const sizeScale = 20/range
+        const sizeOffset = 5
+    
+        const points = viewer.scene.primitives.add(new Cesium.PointPrimitiveCollection());
+
+        // viewer.entities.suspendEvents()
+        for (dataPoint of data){
+          // Add a marker for this point
+          const {lon,lat,alt,val} = dataPoint
+          const scaledVal = (val+valueMin)*scale+offset
+          const sizeScaledVal = (val-valueMin)*sizeScale+sizeOffset
+          const color = Cesium.Color.fromHsl(scaledVal, 0.8, 0.5)
   
+          const pointOptions = {
+            position: Cesium.Cartesian3.fromDegrees(lon,lat,alt),
+            color: color,
+            pixelSize: 10,
+          };
+          points.add(pointOptions)
+        }
+
+        
+        // viewer.entities.resumeEvents()
+      }
+
+    // Call from url
+    loadScatter("/json")
+
+
+      
+
+});
+
+addToolbarButton("Lat Lon Alt Val Scatter as Entities", function () {
+  async function loadScatter(jsonFilename){
+      const response = await fetch(jsonFilename)
+      const data = await response.json()
+  
+      const valueMin = -4.7
+      const valueMax = 6.7
+      const range = valueMax-valueMin
+      const scale = 1/range
+      const offset = 0
+      const sizeScale = 20/range
+      const sizeOffset = 5
+
+      // viewer.entities.suspendEvents()
+      for (dataPoint of data){
+        // Add a marker for this point
+        const {lon,lat,alt,val} = dataPoint
+        const scaledVal = (val+valueMin)*scale+offset
+        const sizeScaledVal = (val-valueMin)*sizeScale+sizeOffset
+        const color = Cesium.Color.fromHsl(scaledVal, 0.8, 0.5)
+      
+        // Add point
+        const entity = new Cesium.Entity({
+          description:`Lat: ${lon} Lon: ${lat} Alt: ${alt} Val ${val}`,
+          position: Cesium.Cartesian3.fromDegrees(lon,lat,alt),
+          point: {pixelSize: 10, color: color}
+        });
+
+        viewer.entities.add(entity)
+
+      }
+
+      
+      // viewer.entities.resumeEvents()
+    }
+
+  // Call from url
+  loadScatter("/json")
+
+
+});
+

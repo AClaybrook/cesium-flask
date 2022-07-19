f6e8621173d31c4936b4fe183ce5a85773f374e9
AClaybrook <48696805+AClaybrook@users.noreply.github.com>
2022-07-19 02:07:26 -0700

    More examples

--- app.py
+++ app.py
@@ -9,15 +9,28 @@ def home():
     return render_template('index.html',data={'online':True})
 
 # Return czml
-@app.route('/czml')
-def get_czml():
-    filename = 'data/data.czml'
+@app.route('/czml1')
+def get_czml1():
+    filename = 'data/data1.czml'
+    return send_file(filename)
+
+# Return czml
+@app.route('/czml2')
+def get_czml2():
+    filename = 'data/data2.czml'
     return send_file(filename)
 
 # Return data 
 @app.route('/json')
 def get_json():
     filename = 'data/data.json'
+    return send_file(filename)
+
+
+# Return Ground Stations 
+@app.route('/GroundStations')
+def get_groundstations():
+    filename = 'data/GroundStations.json'
     return send_file(filename)
 
 # Run if main
--- static/js/CesiumContainer.js
+++ static/js/CesiumContainer.js
@@ -22,7 +22,7 @@ if(online==true){
 }
 
 viewer.scene.globe.enableLighting = true;
-
+viewer.scene.postProcessStages.fxaa.enabled = true
 
 
 // Webpage controls
@@ -60,7 +60,7 @@ function addToolbarButton(text, onclick, toolbarID) {
     button.type = 'button';
     button.className = 'cesium-button';
     button.onclick = function() {
-        reset();
+        // reset();
         onclick();
     };
     button.textContent = text;
@@ -71,7 +71,7 @@ function addToolbarMenu(options, toolbarID) {
     var menu = document.createElement('select');
     menu.className = 'cesium-button';
     menu.onchange = function() {
-        window.Sandcastle.reset();
+        reset();
         var item = options[menu.selectedIndex];
         if (item && typeof item.onselect === 'function') {
             item.onselect();
@@ -83,6 +83,7 @@ function addToolbarMenu(options, toolbarID) {
         var option = document.createElement('option');
         option.textContent = options[i].text;
         option.value = options[i].value;
+        option.onselect = options[i].onselect;
         menu.appendChild(option);
     }
 }
@@ -104,9 +105,15 @@ addToolbarButton("Set time now", function () {
   setTimeNow()
 })
 
+
+function addCZML(czmlRoute){
+  viewer.clock.shouldAnimate = true
+  viewer.dataSources.removeAll()
+  viewer.dataSources.add(Cesium.CzmlDataSource.load(czmlRoute));
+}
+
 addToolbarButton("Czml Datasource", function () {
-    viewer.clock.shouldAnimate = true
-    viewer.dataSources.add(Cesium.CzmlDataSource.load("czml"));
+    addCZML("czml1")
 });
 
 addToolbarButton("Lat Lon Val as Polyline", function () {
@@ -114,19 +121,18 @@ addToolbarButton("Lat Lon Val as Polyline", function () {
         const response = await fetch(jsonFilename)
         const data = await response.json()
     
-        const valueMin = -4.7
-        const valueMax = 6.7
+        const valueMin = 0
+        const valueMax = 1
         const range = valueMax-valueMin
         const scale = 1/range
         const offset = 0
         const sizeScale = 20/range
         const sizeOffset = 5
-        const heightScale = 1000000/range
+        const heightScale = 2000000/range
         const heightOffset = 10000
     
-
         // Create a polyline collection 
-        // const polylines = viewer.scene.primitives.add(new Cesium.PolylineCollection());
+        const polylines = viewer.scene.primitives.add(new Cesium.PolylineCollection());
 
         for (dataPoint of data){
           // Add a marker for this point
@@ -140,38 +146,36 @@ addToolbarButton("Lat Lon Val as Polyline", function () {
           const heightPosition = Cesium.Cartesian3.fromDegrees(lon,lat,heightScaledVal);
     
           // // WebGL Globe only contains lines, so that's the only graphics we create.
-          const polyline = new Cesium.PolylineGraphics();
-          polyline.material = new Cesium.ColorMaterialProperty(color);
-          polyline.width = new Cesium.ConstantProperty(5);
-          polyline.arcType = new Cesium.ConstantProperty(Cesium.ArcType.NONE);
-          polyline.positions = new Cesium.ConstantProperty([
-            surfacePosition,
-            heightPosition,
-          ]);
+          // const polyline = new Cesium.PolylineGraphics();
+          // polyline.material = new Cesium.ColorMaterialProperty(color);
+          // polyline.width = new Cesium.ConstantProperty(5);
+          // polyline.arcType = new Cesium.ConstantProperty(Cesium.ArcType.NONE);
+          // polyline.positions = new Cesium.ConstantProperty([
+          //   surfacePosition,
+          //   heightPosition,
+          // ]);
     
-          //The polyline instance itself needs to be on an entity.
-          const entity = new Cesium.Entity({
-            polyline: polyline,
-            description:`Temp [C] ${val}`,
-          });
+          // //The polyline instance itself needs to be on an entity.
+          // const entity = new Cesium.Entity({
+          //   polyline: polyline,
+          //   description:`Temp [C] ${val}`,
+          // });
     
-          viewer.entities.add(entity)
+          // viewer.entities.add(entity)
 
-          // const material = Cesium.Material.fromType('Color', {color : color});
-
-          // const polylineOptions = {
-          //   material: material,
-          //   width : 5,
-          //   positions : [surfacePosition,heightPosition]
-          // }
-          // polylines.add(polylineOptions)
-    
+          const material = Cesium.Material.fromType('Color', {color : color});
+          const polylineOptions = {
+            material: material,
+            width : 5,
+            positions : [surfacePosition,heightPosition]
+          }
+          polylines.add(polylineOptions)
         }
-
       }
 
 
     // Call from url
+    reset()
     loadPloyline("/json")
 
 });
@@ -181,8 +185,9 @@ addToolbarButton("Lat Lon Alt Val Scatter", function () {
         const response = await fetch(jsonFilename)
         const data = await response.json()
     
-        const valueMin = -4.7
-        const valueMax = 6.7
+        
+        const valueMin = 0
+        const valueMax = 1
         const range = valueMax-valueMin
         const scale = 1/range
         const offset = 0
@@ -220,12 +225,12 @@ addToolbarButton("Lat Lon Alt Val Scatter", function () {
 });
 
 addToolbarButton("Lat Lon Alt Val Scatter as Entities", function () {
-  async function loadScatter(jsonFilename){
+  async function loadScatterEntities(jsonFilename){
       const response = await fetch(jsonFilename)
       const data = await response.json()
   
-      const valueMin = -4.7
-      const valueMax = 6.7
+      const valueMin = 0
+      const valueMax = 1
       const range = valueMax-valueMin
       const scale = 1/range
       const offset = 0
@@ -251,13 +256,45 @@ addToolbarButton("Lat Lon Alt Val Scatter as Entities", function () {
 
       }
 
-      
       // viewer.entities.resumeEvents()
     }
 
   // Call from url
-  loadScatter("/json")
-
+  loadScatterEntities("/json")
 
 });
+
+addToolbarButton("Ground Stations", function () {
+  async function loadScatterEntities(jsonFilename){
+      const response = await fetch(jsonFilename)
+      const data = await response.json()
+  
+      for (dataPoint of data){
+        // Add a marker for this point
+        const {lon,lat,alt,val,desc} = dataPoint
+        const entity = new Cesium.Entity({
+          description:`${desc}`,
+          position: Cesium.Cartesian3.fromDegrees(lon,lat,alt),
+          point: {pixelSize: 10, color: Cesium.Color.WHITE, scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.8, 15000000, 0.5)},
+          label: {
+            text: `${desc}`,
+            translucencyByDistance: new Cesium.NearFarScalar(100000, 1, 4000000, 0.0),
+            scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.8, 15000000, 0.5),
+            pixelOffset: new Cesium.Cartesian2(0.0, -30),
+            font : "12px Helvetica",
+          }                
+
+        });
+        viewer.entities.add(entity)
+      }
+    }
+
+  // Call from url
+  loadScatterEntities("/GroundStations")
+});
+
+var options = [{text:'data1.czml',value:'val1',onselect:function(){addCZML("czml1")}},
+               {text:'data2.czml',value:'val2',onselect: function() {addCZML("czml2")}}]
+addToolbarMenu(options) 
+
 
